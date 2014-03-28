40b3508a2af2dbdc1dd812600de9599f04662c1f
DANDWYERIRL <dwyerdan3@yahoo.ie>
2014-03-28 09:01:08 +0000

    first commit

---
+++ javascripts/Menu.js
@@ -0,0 +1,29 @@
+ // I learn this nice jQuery piece for swapping a nav menu for a drop down box 
+ //here http://css-tricks.com/convert-menu-to-dropdown/
+	 $(function() {
+	   
+      // Create the dropdown base
+      $("<select />").appendTo(".navigation nav");
+      
+      // Create default option "Go to..."
+      $("<option />", {
+         "selected": "selected",
+         "value"   : "",
+         "text"    : "Go to..."
+      }).appendTo("nav select");
+      
+      // Populate dropdown with menu items
+      $("nav a").each(function() {
+       var el = $(this);
+       $("<option />", {
+           "value"   : el.attr("href"),
+           "text"    : el.text()
+       }).appendTo("nav select");
+      });
+      
+	   // To make dropdown actually work
+      $("nav select").change(function() {
+        window.location = $(this).find("option:selected").val();
+      });
+	 
+	 });
