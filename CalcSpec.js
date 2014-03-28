330c97b308c05095662836ea70bcf8073035727f
DANDWYERIRL <dwyerdan3@yahoo.ie>
2014-03-28 11:12:17 +0000

    fourth commit
    should include error handling

---
+++ CalcSpec.js
@@ -0,0 +1,7 @@
+describe("LengthFunction", function() {
+it("should be able to convert cm to inch and inch to cm", function() {
+    expect(var)toBe()
+  });
+
+
+}
--- javascripts/Calc.js
+++ javascripts/Calc.js
@@ -1,20 +1,44 @@
 
 //function to convert the lengths  
-function LengthFunction(Inch){
-var Inch = document.getElementById("Inch").value*2.54;
-document.getElementById("Inch").value=Inch;
-
-var Cm = document.getElementById("Cm").value/2.54;
-document.getElementById("Cm").value=Cm;
-}
+	function LengthFunction(Inch){
+	
+		//var Inch = document.getElementById("Inch").value;
+			if (inch > -1){
+				var ResultIN = Inch*2.54;
+				document.getElementById("Inch").value=ResultIN;
+			}
+			
+		var cm = document.getElementById("Cm").value;
+			if(cm > -1){
+				var ResultCm = cm/2.54;
+				document.getElementById("Cm").value=ResultCm;
+			}
+			
+			else{
+				alert("Please enter a correct value")
+			}
+	}
 
 // function to convert the Temps 
-function TempFunction(){
-
-var Cel = document.getElementById("Cel").value * 9 / 5 + 32;
-document.getElementById("Cel").value = Cel;
-
-var Far = (document.getElementById("Far").value -32) * 5 / 9;
-document.getElementById("Far").value = Far;
-
-}
+	function TempFunction(){
+	
+		var Cel = document.getElementById("Cel").value
+			if(Cel > -273){
+				var ResultCel = Cel * 9 / 5 + 32;
+				document.getElementById("Cel").value = ResultCel;
+			}
+			
+		var Far = document.getElementById("Far").value
+			if(Far >-1000){
+				var ResultFar = (Far -32) * 5 / 9;
+				document.getElementById("Far").value = ResultFar;
+			}		
+			else{
+				alert("enter a correct value")
+			}
+	}
+	
+	
+	
+	
+	
